356206c60a36db0d77c5335b840dc03dfeca26e7
Ruben_Jaramillo <tiburones.tk@gmail.com>
2021-05-02 19:50:22 -0500

    Se termino la primera columna de imagenes y el modal en la seccion portfolio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const $portfolio = document.querySelector(".sec-portfolio-js");
+const $modalImgPortfolio = document.querySelector(".img-modal-js");
 
-$portfolio.addEventListener("click", (e) => {
-   //console.log(e.target)
-   console.log(e.target.classList);
-});
-
-document.getElementById('app').innerHTML = 'Hola mundo'
+$portfolio.addEventListener('click', (e) => {
+   //console.log(e.target.classList);
+   if(e.target.classList.contains('img-btn-modal-js')) {
+      // SRC
+      let urlImg = e.target.attributes[0].nodeValue;
+      //Add Modal
+      $modalImgPortfolio.src = urlImg;
+   }
+})
