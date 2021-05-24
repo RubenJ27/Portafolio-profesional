1b1906e3309b1472989a416d71d5f846279e57fb
Ruben_Jaramillo <tiburones.tk@gmail.com>
2021-05-24 16:52:00 -0500

    Se uso observer api para con vanilla js implementar el indicador de seccion menu con el scroll

--- js/script.js
+++ js/script.js
@@ -10,3 +10,39 @@ $portfolio.addEventListener('click', (e) => {
       $modalImgPortfolio.src = urlImg;
    }
 })
+
+addEventListener('DOMContentLoaded', () => {
+   const btn_menu = document.querySelector('.btn_menu')
+   if(btn_menu){
+       btn_menu.addEventListener('click', () => {
+           const menu_items = document.querySelector('menu_items')
+           menu_items.classList.toggle('mostrar')
+       })
+   }
+   /*Indicador de menu*/
+
+   const secciones = document.querySelectorAll('.seccion')
+   const menuItems = document.querySelectorAll('.menu_item')
+
+   const funcionObserver = entries => {
+       entries.forEach(entry => {
+           if (entry.isIntersecting){
+               const itemActual = Array.from(menuItems).find(item => item.dataset.url === entry.target.id)
+               itemActual.classList.add('active')
+               for (const item of menuItems) {
+                   if (item != itemActual){
+                       item.classList.remove('active')
+                   }
+               }
+           }
+       })
+   }
+
+   const observer = new IntersectionObserver(funcionObserver, {
+       root: null,
+       rootMargin: '0px',
+       threshold: 0.1
+   })
+
+   secciones.forEach(seccion => observer.observe(seccion))
+})
