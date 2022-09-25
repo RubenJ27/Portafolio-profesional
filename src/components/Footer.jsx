814ec1193be0fe9f065f9b85da4138513652a8ad
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-09-25 03:51:33 -0500

    fix: :bug: se esta en pruebas de resolucion del footer
    Se esta validando resolucion del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <>
       <footer className="flex flex-wrap items-center justify-center px-2 py-3 bg-primary-color flex-end w-full relative bottom-0">
-        <div className="md:container flex flex-row items-center justify-center">
+        <div className="container md:container flex flex-row items-center justify-center">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <div className="flex w-full"> <p
-              className="tall:bg-red-700 tall:text-[1.8vh] text-[2.7vh] sm:text-sm md:text-sm lg:text-2xl font-light leading-relaxed whitespace-nowrap text-white items-center"
+              className="tall:text-[1.8vh] text-[2.7vh] sm:text-sm md:text-sm lg:text-2xl font-light leading-relaxed whitespace-nowrap text-white items-center"
             >Ruben Jaramillo Copyright © 2022 todos los derechos reservados. 
             </p></div>
            
