572bf0897950e2c7b434cb1bd2589a7a62ce0384
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-09-25 03:40:48 -0500

    fix: :construction: Se esta validando la resolucion de footer
    se esta validando el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,14 @@ export default function Footer() {
       <footer className="flex flex-wrap items-center justify-center px-2 py-3 bg-primary-color flex-end w-full relative bottom-0">
         <div className="md:container flex flex-row items-center justify-center">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <div className="flex"> <p
-              className="tall:text-[1.8vh] text-[2.7vh] sm:text-sm md:text-sm lg:text-2xl font-light leading-relaxed inline-block whitespace-nowrap text-white items-center"
+            <div className="flex w-full"> <p
+              className="tall:bg-red-700 tall:text-[1.8vh] text-[2.7vh] sm:text-sm md:text-sm lg:text-2xl font-light leading-relaxed whitespace-nowrap text-white items-center"
             >Ruben Jaramillo Copyright © 2022 todos los derechos reservados. 
             </p></div>
            
           </div>
 
-          <div
-            className={
+          <div className={
               "flex lg:flex w-[50%] flex-grow items-center justify-end"
             }
             id="example-navbar-danger"
