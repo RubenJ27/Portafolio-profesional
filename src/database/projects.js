9588fb61b805bd0c706dc4f4e2ac8f97d07951ae
Ruben Jaramillo <tiburones.tk@gmail.com>
2023-08-23 20:51:12 -0500

    feat: :sparkles: Updated the movie api image v2.0 and changed the description a bit
    Updated the movie api image v2.0 and changed the description a bit

--- src/database/projects.js
+++ src/database/projects.js
@@ -1,50 +1,52 @@
 export const projects = [
   {
     id: 1,
-    title: 'To Do List',
-    description: 'Aplicación To Do List para gestión de tareas realizada con react',
-    url: 'To-Do-Img.png',
-    url_github: 'https://github.com/RubenJ27/M4U3To-Do-RubenJaramillo',
-    url_live: 'https://storied-moonbeam-f41e99.netlify.app/'
+    title: "To Do List",
+    description: "Aplicación To Do List para gestión de tareas realizada con react",
+    url: "To-Do-Img.png",
+    url_github: "https://github.com/RubenJ27/M4U3To-Do-RubenJaramillo",
+    url_live: "https://storied-moonbeam-f41e99.netlify.app/",
   },
   {
     id: 2,
-    title: 'API del clima',
-    description: 'Aquí está el link directo que incluye el despliegue de la aplicación del clima',
-    url: 'App-clima-ts.PNG',
-    url_github: 'https://github.com/RubenJ27/App-clima-typescript',
-    url_live: 'https://app-clima-ts.netlify.app/'
+    title: "API del clima",
+    description: "Aquí está el link directo que incluye el despliegue de la aplicación del clima",
+    url: "App-clima-ts.PNG",
+    url_github: "https://github.com/RubenJ27/App-clima-typescript",
+    url_live: "https://app-clima-ts.netlify.app/",
   },
   {
     id: 3,
-    title: 'API de super heroes',
-    description: 'Aquí está el link directo que incluye el despliegue de la aplicación de super heroes',
-    url: 'Api-heroes-img.png',
-    url_github: 'https://github.com/RubenJ27/Api-superheroes',
-    url_live: 'https://api-heroes.netlify.app/'
+    title: "API de super heroes",
+    description: "Aquí está el link directo que incluye el despliegue de la aplicación de super heroes",
+    url: "Api-heroes-img.png",
+    url_github: "https://github.com/RubenJ27/Api-superheroes",
+    url_live: "https://api-heroes.netlify.app/",
   },
   {
     id: 4,
-    title: 'Landing page',
-    description: 'Aquí está el link directo que incluye el despliegue de la página de la lading page con react',
-    url: 'Landing-Page-Img.png',
-    url_github: 'https://github.com/RubenJ27/Portafolio-profesional',
-    url_live: 'https://landing-page-ruben-jaramillo.web.app/'
+    title: "Landing page",
+    description: "Aquí está el link directo que incluye el despliegue de la página de la lading page con react",
+    url: "Landing-Page-Img.png",
+    url_github: "https://github.com/RubenJ27/Portafolio-profesional",
+    url_live: "https://landing-page-ruben-jaramillo.web.app/",
   },
   {
     id: 5,
-    title: 'E commerce react',
-    description: 'Aquí hay un enlace directo que incluye la creación de una página de e-commerce con react y redux-toolkit',
-    url: 'E-commerce-react.png',
-    url_github: 'https://github.com/RubenJ27/E-commerce-react',
-    url_live: 'https://e-commerce-shop-shoes.netlify.app/'
+    title: "E commerce react",
+    description:
+      "Aquí hay un enlace directo que incluye la creación de una página de e-commerce con react y redux-toolkit",
+    url: "E-commerce-react.png",
+    url_github: "https://github.com/RubenJ27/E-commerce-react",
+    url_live: "https://e-commerce-shop-shoes.netlify.app/",
   },
   {
     id: 6,
-    title: 'Pagina para buscar peliculas',
-    description: 'Aquí hay un enlace directo que incluye la creación de una página de búsqueda de películas',
-    url: 'App-movies-redux-toolkit.png',
-    url_github: 'https://github.com/RubenJ27/App-peliculas',
-    url_live: 'https://page-to-see-movie-information.netlify.app/'
-  }
-]
+    title: "Pagina para buscar peliculas",
+    description:
+      "Aquí hay un enlace directo que incluye la creación de una página de búsqueda de películas que se actualizo a la v2.0",
+    url: "App-movies-v2.0.png",
+    url_github: "https://github.com/RubenJ27/App-peliculas",
+    url_live: "https://page-to-see-movie-information.netlify.app/",
+  },
+];
